f34fc4408629f625ec1e7549dd5cb868d64f0ddd
AshGasparyanCS <ashotg2@uci.edu>
2025-03-17 00:25:51 +0000

    gen word ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,66 +1,53 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <set>
-#include <queue>
-#include <unordered_set>
 #include "ladder.h"
 
 using namespace std;
 
 bool is_adjacent(const string& word1, const string& word2)
 {
-    if (word1.length() != word2.length())
-    {
-        return false;
-    }
-    int diff_count = 0;
-    for (size_t i = 0; i < word1.length(); i++)
-    {
-        if (word1[i] != word2[i])
-        {
-            diff_count++;
-            if (diff_count > 1)
-            {
-                return false;
-            }
-        }
-    }
-    return diff_count == 1;
+    return edit_distance_within(word1, word2, 1);
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list)
 {
+    if(begin_word == end_word)
+    {
+        return {begin_word};
+    }
+    if(word_list.find(end_word) == word_list.end())
+    {
+        error(begin_word, end_word, "End word not in word list.")
+        return{};
+    }
+
     queue<vector<string>> ladder_queue;
     ladder_queue.push({begin_word});
 
-    unordered_set<string> visited;
+    set<string> visited;
     visited.insert(begin_word);
 
     while(!ladder_queue.empty())
     {
-        vector<string> ladder = ladder_queue.front();
-        ladder_queue.pop
-        string last_word = ladder.back();
-        for(const string& word : word_list)
+        vector<string> current_ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = current_ladder.back();
+
+        for(const string& candidate : word_list)
         {
-            if(is_adjacent(last_word, word))
+            if(is_adjacent(last_word, candidate) && !visited.count(candidate))
             {
-                if(visited.find(word) == visited.end())
+                vector<string> new_ladder = current_ladder;
+                new_ladder.push_back(candidate);
+                if (candidate == end_word)
                 {
-                    visited.insert(word);
-
-                    vector<string> new_ladder = ladder;
-                    new_ladder.push_back(word);
-
-                    if (word == end_word)
-                    {
-                        return new_ladder;
-                    }
-                    ladder_queue.push(new_ladder);
+                    return new_ladder;
                 }
+
+                visited.insert(candidate);
+                ladder_queue.push(new_ladder);
             }
         }
     }
+
+    error(begin_word, end_word, "No ladder found.");
     return {};
 }
